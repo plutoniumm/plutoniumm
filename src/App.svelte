19b0ec616a6bfcd1751836a8cb7d51061f2b8a3d
Manav Seksaria <vivaldi@Gojira.local>
2020-10-14 16:21:05 +0530

    Svelte init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import CenterBox from "./components/centerbox.svelte";
+</script>
+
+<style>
+	main {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		background: #00f;
+	}
+</style>
+
+<main>
+	<CenterBox />
+</main>
