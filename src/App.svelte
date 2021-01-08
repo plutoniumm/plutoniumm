d6b48ad1df1a23957d80cd33d46372d8679243c0
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2021-01-08 11:08:41 +0530

    Same kaam ke kam lines

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,10 @@
 	import { fade } from "svelte/transition";
 
 	$: code = false;
-	const codeCg = () => {
-		code = !code;
-	};
+	const codeCg = () => (code = !code);
 </script>
 
-<style>
+<style type="text/scss">
 	main {
 		width: 100%;
 		height: 100%;
@@ -18,18 +16,48 @@
 		align-items: center;
 		display: flex;
 		background: #cae1ff;
+		.section {
+			height: auto;
+			width: 480px;
+			transition: all 0.3s ease;
+		}
+		.code {
+			text-align: center;
+			padding: 0.75em;
+			color: #aaa8;
+			svg {
+				border: 1px solid #8880;
+				padding: 0.25em;
+			}
+			&:hover {
+				color: #222;
+				cursor: pointer;
+				transition: color 0.3s ease;
+			}
+		}
+	}
+	@media (max-width: 600px) {
+		.section {
+			width: 300px;
+		}
 	}
 </style>
 
 <main>
-	{#if !code}
-		<div in:fade>
-			<CenterBox {codeCg} />
+	<section style="display:flex;flex-direction:column;">
+		<div class="code" on:click={codeCg}>
+			<svg viewBox="0 0 32 32" width="32" height="32" fill="none">
+				<path
+					d={!code ? 'M22 11 C22 16 19 20 16 20 13 20 10 16 10 11 10 6 12 3 16 3 20 3 22 6 22 11 Z M4 30 L28 30 C28 21 22 20 16 20 10 20 4 21 4 30 Z' : 'M10 9 L3 17 10 25 M22 9 L29 17 22 25 M18 7 L14 27'} />
+			</svg>
 		</div>
-	{/if}
-	{#if code}
-		<div in:fade>
-			<CodeBox {codeCg} />
+		<div class="section" style={!code ? '' : 'flex-wrap:wrap'}>
+			{#if !code}
+				<CenterBox />
+			{/if}
+			{#if code}
+				<CodeBox />
+			{/if}
 		</div>
-	{/if}
+	</section>
 </main>
--- src/components/centerbox.svelte
+++ src/components/centerbox.svelte
@@ -1,64 +1,37 @@
-<script>
-      export let codeCg;
-</script>
-
 <style type="text/scss">
-      .section {
-            width: 500px;
-            height: auto;
-            .left {
-                  padding-right: 20px;
-                  img {
-                        width: 200px;
-                        height: 200px;
-                        border-radius: 120px;
-                        object-fit: cover;
-                  }
-            }
-            .title {
-                  font-size: 32px;
-                  font-weight: 600;
-                  line-height: 8px;
-                  padding: 0;
-            }
-            a {
-                  pointer-events: all;
-                  color: #7eb6ff;
-                  text-decoration: none;
+      .left {
+            padding-right: 20px;
+            img {
+                  width: 200px;
+                  height: 200px;
+                  border-radius: 120px;
+                  object-fit: cover;
             }
       }
+      .title {
+            font-size: 32px;
+            font-weight: 600;
+            line-height: 8px;
+            padding: 0;
+      }
       @media (max-width: 600px) {
-            .section {
-                  width: 325px;
-                  height: auto;
-                  .left,
-                  .right {
-                        width: 100%;
-                        padding: 0;
-                  }
+            .left,
+            .right {
+                  width: 100%;
+                  padding: 0;
             }
       }
 </style>
 
-<section style="display:flex;flex-direction:column;">
-      <div class="code" on:click={codeCg}>
-            <svg viewBox="0 0 32 32" width="32" height="32" fill="none">
-                  <path d="M10 9 L3 17 10 25 M22 9 L29 17 22 25 M18 7 L14 27" />
-            </svg>
+<div class="left"><img src="./assets/pic.jpeg" alt="" /></div>
+<div class="right">
+      <p class="title">Manav Seksaria</p>
+      <p>Cyber Security Researcher <br /> Full Stack Javascript</p>
+      <div style="line-height:1.5em;color: #7eb6ff;pointer-events: all;">
+            <a href="https://frontier.nukes.in">@frontier</a>
+            <br />
+            <a href="https://github.com/plutoniumm">@plutoniumm</a>
+            <br />
+            <a href="mailto:plutoniumm@icloud.com">plutoniumm@icloud.com</a>
       </div>
-      <div class="section">
-            <div class="left"><img src="./assets/pic.jpeg" alt="" /></div>
-            <div class="right">
-                  <p class="title">Manav Seksaria</p>
-                  <p>Cyber Security Researcher, Full Stack Javascript</p>
-                  <div style="line-height:1.5em">
-                        <a href="https://frontier.nukes.in">@frontier</a>
-                        <br />
-                        <a href="https://github.com/plutoniumm">@plutoniumm</a>
-                        <br />
-                        <a
-                              href="mailto:plutoniumm@icloud.com">plutoniumm@icloud.com</a>
-                  </div>
-            </div>
-      </div>
-</section>
+</div>
