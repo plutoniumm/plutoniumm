5ee7089e6a3905702b30b8d0551badd38613d4b3
Manav <51614345+plutoniumm@users.noreply.github.com>
2022-08-08 06:02:48 +0530

    patch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@ npx gh-pages -d build -t true;
 read -p "Add Commit and Push to Github? (y/n) " -n 1 -r;
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "\n";
-    read -p "Commit Message: " -n 100 -r;
+    read -p "Commit Message: " msg;
 
     git add .;
-    git commit -m "$REPLY";
+    git commit -m "$msg";
     git push;
 fi;
