33fd8a3f3ff379e08c828072b6a2ac78ce735bb8
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-04-20 06:58:25 +0530

    first bench

--- src/routes/password/+page.svelte
+++ src/routes/password/+page.svelte
@@ -1,86 +1,88 @@
 <script>
   import { onMount } from "svelte";
-  import PASSWDS from "../../../secrets/passwords.csv";
+  import { hash } from "predefined";
+  import { chunked, getQRGen } from "./utils";
   import { encrypt, decrypt } from "./encc";
-  import { PASSWORD, email_replaces } from "../../../secrets/password.js";
-  import QR from "qr-code-styling";
+  import { PASSWORD, images } from "../../../secrets/password.js";
 
-  const emailReplace = (username) => {
-    for (const [key, value] of Object.entries(email_replaces))
-      username = username.replace(key, value);
+  const prefix = "https://nukes.in/password?encc=";
 
-    return username;
-  };
-
-  const chunked = PASSWDS.map(({ name, url, username, password }) => {
-    return {
-      name: name
-        .replace(".com", "")
-        .replace(".in", "")
-        .replace(".me", "")
-        .replace(".org", ""),
-      username: emailReplace(username),
-      password,
-      url: new URL(url).hostname,
-      value: (Math.random() * 1e4) | 0,
-    };
-  })
-    .sort((a, b) => a.value - b.value)
-    .reduce((all, one, i) => {
-      const ch = Math.floor(i / 4);
-      all[ch] = [].concat(all[ch] || [], one);
-      return all;
-    }, []);
-
-  const password = PASSWORD;
-
-  const generateQR = (data, query) => {
-    const qrCode = new QR({
-      width: 300,
-      height: 300,
-      data,
-      image: null,
-      type: "svg",
-      dotsOptions: { color: "#46b", type: "rounded" },
-      backgroundOptions: { color: "#0000" },
-      imageOptions: { crossOrigin: "anonymous", margin: 0 },
-    });
-
+  const decr_uri = (data) => prefix + encodeURIComponent(data);
+  const generateQR = (encrypted, query) => {
+    const data = decr_uri(encrypted);
     const el = document.getElementById(query);
-    qrCode.getRawData("png").then((png) => {
-      const blob = new Blob([png], { type: "image/png" });
-      const image = new Image();
+    getQRGen(data)
+      .getRawData("png")
+      .then((png) => {
+        const image = new Image();
 
-      image.src = URL.createObjectURL(blob);
-      image.onload = () => el.appendChild(image);
-    });
-
-    return "";
+        image.src = URL.createObjectURL(new Blob([png], { type: "image/png" }));
+        image.onload = () => el.appendChild(image);
+      });
+    return encrypted;
   };
 
-  onMount(async () => {});
+  onMount(async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const encrypted = decodeURIComponent(urlParams.get("encc"));
+
+    if (encrypted?.length > 5) {
+      console.log(encrypted);
+      const passkey = await hash(prompt("Enter passkey"));
+
+      const decrypted = await decrypt(encrypted, passkey);
+      console.log(decrypted);
+    }
+  });
 </script>
 
 <!-- Path: src/routes/password/+page.svelte -->
 <div class="w-100 f fw" style="word-wrap: break-word;">
   {#each chunked as chunk, index}
     {@const id = `canvas-${index}`}
-    {@const reduced = chunk.map(({ name, username, password }) => {
-      return [name, username, password];
-    })}
+    {@const reduced = chunk.map(({ name, username, password }) => [
+      name,
+      username,
+      password,
+    ])}
+    {@const rand = (Math.random() * 20) | 0}
     {@const string = JSON.stringify(reduced).replaceAll("www.", "")}
 
     <div class="f-col tc p10">
       <div {id} />
-      {#await encrypt(string, password) then encrypted}
-        <div class="fw7">{encrypted.length}</div>
+      {#await encrypt(string, PASSWORD) then encrypted}
+        <a class="p5 rx10 mx-a fw7 button" href={decr_uri(encrypted)}>
+          Decrypt {generateQR(encrypted, id).length + rand}
+        </a>
         <div>
-          {generateQR(encrypted, id)}
           {#each reduced as name}
-            {name[0]} - {name[1]}<br />
+            {#if Object.hasOwn(images, name[0])}
+              <img class="p5 rx10" src={images[name[0]]} alt="name[0]" />
+            {:else}
+              <a href={name[0]}>{name[0]} </a>
+            {/if}
           {/each}
         </div>
       {/await}
     </div>
   {/each}
 </div>
+
+<style>
+  a,
+  a:visited,
+  a:hover {
+    color: purple !important;
+  }
+  img {
+    object-fit: contain;
+    height: 48px;
+    width: 48px;
+  }
+  .button {
+    background: #fff;
+    border: 1px solid #0002;
+    max-width: 200px;
+    margin-bottom: 10px;
+  }
+</style>
