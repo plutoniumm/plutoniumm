4d870a525d5659ce1ffc814dc4eff496de5658c0
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-06-24 05:52:35 +0530

    add doc

--- src/routes/docs/[slug]/+page.svelte
+++ src/routes/docs/[slug]/+page.svelte
@@ -2,16 +2,29 @@
   export let data;
 </script>
 
-<div class="p20">
-  <article class="blur-fff8 rpm-10 mx-a" style="max-width:991px;">
-    <svelte:component this={data.component} />
-  </article>
-</div>
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/PrismJS/prism-themes@1.9.0/themes/prism-vsc-dark-plus.css"
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+  <style>
+    pre {
+      border-radius: 10px;
+    }
+  </style>
+</svelte:head>
+
+<article class="blur-fff8 rx10 p10" style="max-width:991px;">
+  <svelte:component this={data.component} />
+</article>
 
 <style>
   .blur-fff8 {
+    margin: 20px auto;
     overflow: hidden;
     word-wrap: break-word;
-    width: calc(min(70ch, 100%) - 40px);
+    width: calc(min(70ch, 100%) - 60px);
   }
 </style>
