07bf4af869c7fe8ef5df4f170b68c810cf2ebcce
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-09-17 12:53:40 +0530

    Updates

---
+++ _app/immutable/chunks/8-aa34f78c.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_page-47280627.js";import{default as s}from"../components/pages/t/_slug_/_page.svelte-0fc8558f.js";const e=!0;export{s as component,e as has_server_load,t as universal};
